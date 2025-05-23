e40d9beff960e06fea3136fd7cdc97dc3014465c
Kieran <Kierancoder@gmail.com>
2025-05-24 00:35:37 +0100

    test

--- index.js
+++ index.js
@@ -88,9 +88,8 @@ function isAuthenticated(req, res, next) {
 
 function isAdmin(req, res, next) {
   if (req.session.role === 'admin') return next();
-  return res.status(403).json({ error: "Access denied: Admins only" });
+  return res.status(403).send("Access denied: Admins only");
 }
-
 
 // ==== AUTH ROUTES ====
 
